0fa88c1d9dacf3ac273932bbaea33880befa4fa0
cfalcondair <cfalcondair@gmail.com>
2018-05-29 19:10:31 +0100

    Solution to Bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,15 +1,31 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package bob responds to statements in rule based way
+// based on the content of the statement said to bob.
 package bob
 
-// Hey should have a comment documenting it.
+import (
+	"regexp"
+)
+
+// Hey takes a remark and returns what Bob
+// says to the remark.
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	question := regexp.MustCompile("\\?\\s*$")
+	yell := regexp.MustCompile("^[!\\^*\\($#@%0-9A-Z ,]+!$")
+	yellNoExclamation := regexp.MustCompile("^[A-Z ]+$")
+	gibberish := regexp.MustCompile("^[A-Z]+$")
+	silence := regexp.MustCompile("^\\s*$")
+	yellQuestion := regexp.MustCompile("^[A-Z ,]+\\?$")
+
+	switch {
+	case silence.MatchString(remark):
+		return "Fine. Be that way!"
+	case yellQuestion.MatchString(remark):
+		return "Calm down, I know what I'm doing!"
+	case yell.MatchString(remark) || gibberish.MatchString(remark) || yellNoExclamation.MatchString(remark):
+		return "Whoa, chill out!"
+	case question.MatchString(remark):
+		return "Sure."
+	default:
+		return "Whatever."
+	}
 }
